12dd39bd8e99ad22eef3a0319ff793cbb9a0da03
Nanashikung <nata.station@hotmail.com>
2023-02-21 15:06:42 +0700

    update parser version recognizer with no test

--- Parser/ExprParser.java
+++ Parser/ExprParser.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class ExprParser implements Parser {
 
-    private  Tokenizer tkz;
+    private final Tokenizer tkz;
 
     public  ExprParser(Tokenizer tkz){
         this.tkz = tkz;
@@ -10,37 +10,35 @@ public class ExprParser implements Parser {
 
     @Override
     public void parse() throws SyntaxError {
-        System.out.println("before plan");
-            parsePlan();
-//            if(tkz.hasNextToken()){
-//                throw new SyntaxError(tkz.peek());
-//            }
-            
-
+        System.out.println("plan");
+        parsePlan();
     }
 
     private void parsePlan() throws SyntaxError{
-        System.out.println("before statement");
-        parseStament();
+        System.out.println("state");
+        parseStatement();
     }
 
-    private void parseStament() {
-//        if(tkz.peek("Command"))
-//        parseCommand();
-//        else if (tkz.peek("{")) {
-//            tkz.consume();
-//            parseBlockStament();
-//            if(tkz.peek("}")) tkz.consume();
-//            else throw new SyntaxError("expected : " + "}");
-//        }
-        System.out.println("before ifstatement");
-        parseIfStatement();
-//        parseWhileStatement();
-
+    private void parseStatement() {
+        System.out.println("in state");
+        if(tkz.peek("if")){
+            tkz.consume();
+            parseIfStatement();
+        } else if (tkz.peek("{")) {
+            tkz.consume();
+            parseBlockStatement();
+            if(tkz.peek("}")) tkz.consume();
+            else throw new SyntaxError("expected : " + "}");
+        } else if (tkz.peek("while")) {
+            parseWhileStatement();
+        }else{
+            System.out.println("before command");
+            parseCommand();
+        }
     }
 
-    private void parseBlockStament() {
-        parseStament();
+    private void parseBlockStatement() {
+        parseStatement();
     }
 
     private void parseWhileStatement() {
@@ -48,11 +46,10 @@ public class ExprParser implements Parser {
             tkz.consume();
             if(tkz.peek("(")){
                 tkz.consume();
-                parseStament();
+                parseExpression();
                 if(tkz.peek(")")) tkz.consume();
-                parseStament();
+                parseStatement();
             }
-
         }
     }
 
@@ -63,7 +60,7 @@ public class ExprParser implements Parser {
 
             if(tkz.peek("(")){
                 tkz.consume();
-//                parseExpression();
+                parseExpression();
                 if(!tkz.hasNextToken() ){
                     throw new SyntaxError("expected : " + ")");
                 } else if (tkz.peek(")")) {
@@ -72,29 +69,36 @@ public class ExprParser implements Parser {
             }
             if(tkz.peek("then")){
                 tkz.consume();
-                parseStament();
+                parseStatement();
             }
             if(tkz.peek("else")){
                 tkz.consume();
-                parseStament();
+                parseStatement();
             }
         }
     }
 
     private void parseCommand() {
+        System.out.println("in cm");
         parseAssignmentStatement();
-        parseActionCommand();
+//        parseActionCommand();
     }
 
     private void parseActionCommand() {
         if(tkz.peek("done")){
             //done
-        } else if (tkz.peek("relocate")) {
+            tkz.consume();
+        }else if(tkz.peek("relocate")) {
             //relocate
-        }
-        parseMoveCommand();
-        parseRegionCommand();
-        parseAttackCommand();
+            tkz.consume();
+        }else if(tkz.peek("move")){
+            parseMoveCommand();
+        } else if (tkz.peek("invest") || tkz.peek("collect")) {
+            parseRegionCommand();
+        } else if (tkz.peek("shoot")) {
+            parseAttackCommand();
+        }else throw new SyntaxError("unknown action");
+
     }
 
     private void parseAttackCommand() {
@@ -119,6 +123,7 @@ public class ExprParser implements Parser {
     private void parseMoveCommand() {
         if(tkz.peek("move")){
             //move
+            tkz.consume();
             parseDirection();
         }
         
@@ -127,29 +132,53 @@ public class ExprParser implements Parser {
     private void parseDirection() {
         if(tkz.peek("up")){
             //Up
+            tkz.consume();
         } else if (tkz.peek("down")) {
             //down
+            tkz.consume();
         } else if (tkz.peek("upleft")) {
             //upleft
+            tkz.consume();
         } else if (tkz.peek("upright")) {
             //upright
+            tkz.consume();
         } else if (tkz.peek("downleft")) {
             //downleft
+            tkz.consume();
         } else if (tkz.peek("downright")) {
             //downright
+            tkz.consume();
         }else throw new SyntaxError("unknown direction");
     }
 
 
     private void parseAssignmentStatement() {
+        System.out.println("in assign");
+        String identifier = tkz.consume();
+        if(tkz.peek("=")) {
+            System.out.println("in check =");
+            System.out.println(tkz.peek());
+            tkz.consume();
+        }else throw new SyntaxError("expected =");
         parseExpression();
     }
 
     private void parseExpression() {
+        System.out.println("exp");
         parseTerm();
+        while(tkz.hasNextToken() && (tkz.peek("+") || tkz.peek("-"))){
+            if(tkz.peek("+")){
+                tkz.consume();
+                parseExpression();
+            } else if (tkz.peek("-")) {
+                tkz.consume();
+                parseExpression();
+            }
+        }
     }   
 
     private void parseTerm() {
+        System.out.println("in term");
         parseFactor();
         while (tkz.hasNextToken() && (tkz.peek("*") || tkz.peek("/") || tkz.peek("%"))){
             if(tkz.peek("*")){
@@ -166,29 +195,37 @@ public class ExprParser implements Parser {
     }
 
     private void parseFactor() {
+        System.out.println("in fac");
         parsePower();
-        if(tkz.peek("^")){
+        if(tkz.hasNextToken() && tkz.peek("^")){
             tkz.consume();
             parseFactor();
         }
     }
 
     private void parsePower() {
-        int sth;
-        String sone;
-        if(tkz.peek("(")){
-            parseExpression();
+        System.out.println("in power");
+        if(Character.isDigit((tkz.peek().charAt(0)))){
+            tkz.consume();
+            System.out.println("now in digit");
+        } else if (Character.isAlphabetic(tkz.peek().charAt(0))) {
+            tkz.consume();
         }
-        parseInfoExpression();
-
+        if(tkz.hasNextToken() && tkz.peek("(")){
+            tkz.consume("(");
+            parseExpression();
+            tkz.consume(")");
+        }
+        if(tkz.hasNextToken()) parseInfoExpression();
     }
 
     private void parseInfoExpression() {
-        //Opponent();
-        //parseNearby();
+        if(tkz.peek("opponent")){
+            tkz.consume();
+        } else if (tkz.peek("nearby")) {
+            tkz.consume();
+            parseDirection();
+        }
     }
-
-
-
 
 }
